1b758e96ad411a7932a3d6f68e80486503ee0feb
yref-boop <regofdez.yago@gmail.com>
2022-09-20 15:50:56 +0200

    feat: 3 new waves + energy implemented

--- practicatransmision.m
+++ practicatransmision.m
@@ -1,41 +1,50 @@
 %%%%%%           SISTEMA DE TRANSMISION BANDA BASE         %%%%%%
 
-
 clear all;
 close all;
 
-%=================== Parametros ==================================
-N=10;		 % Periodo de simbolo
-L=10;		 % Numero de bits a transmitir
-tipopulso=1; %1: pulso rectangular 
+%=================== parametros ==================================
+
+N=10;               % periodo de simbolo
+L=10;               % numero de bits a transmitir
+tipopulso=1;        % 1: pulso dado
         
 
+%=================== generacion del pulso =========================
 
-%=================== Generacion del pulso =========================
+n = 0 : N-1;        % n size
+pulso = zeros(1,N); % default pulse to 0
 
-if tipopulso == 1  %pulso rectangular
-  n=0:N-1;
-  pulso=ones(1,N); 
-elseif tipopulso == 2 %escrina un elseif por cda tipo
+if tipopulso == 1       % N rectangular
+    pulso (:) = 1;
+elseif tipopulso == 2   % N/2 +1 pulse
+    pulso (1:N/2) = 1;
+elseif tipopulso == 3   % N/2 +-1 pulse
+    pulso (1:N/2) = 1;
+    pulso (N/2 +1:end) = -1;
+elseif tipopulso == 4   % escala
+    pulso = linspace(0, 1, N);
 end;
 
 
-%=================== Calculo de la energia del pulso =============
-%Escriba el codigo para calcular la energia
+%=================== calculo de la energia del pulso =============
+% escriba el codigo para calcular la energia del pulso
 
-%=================== Generacion de la senal (modulacion) =========
+Ep = sum(abs(pulso).^2);
 
-bits=rand(1,L) < 0.5; %genera 0 y 1 a partir de un vector de numeros 
-                      %aleatorios con distribucion uniforme
+%=================== generacion de la senal (modulacion) =========
 
-%Escriba un bucle que asocie un pulso con amplitud positiva a 0 y 
-%un pulso con amplitud negativa a 1
+bits=rand(1,L) < 0.5; % genera 0 y 1 a partir de un vector de numeros 
+                      % aleatorios con distribucion uniforme
 
-%=================== Representacion grafica ===================
+% escriba un bucle que asocie un pulso con amplitud positiva a 0 y 
+% un pulso con amplitud negativa a 1
+
+%=================== representacion grafica ===================
 figure(1)
 plot(n,pulso);
 title('Pulso transmitido: p(n)');
 grid;
  
 figure(2)
-%Escriba el codigo para representar la senal 
+% escriba el codigo para representar la senal 
