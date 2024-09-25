fdf0f1c380610b36726f50e337290c6122307aac
yref.boop <yref.boop@gmail.com>
2024-09-25 09:06:18 +0200

    chore: code cleanup

--- transmision.m
+++ transmision.m
@@ -1,69 +1,57 @@
-%%%%%%           SISTEMA DE TRANSMISION BANDA BASE         %%%%%%
-
+%% TRANSMISIÓN BANDA BASE
 
 clear all;
 close all;
 
-%=================== Parámetros ==================================
-N=10;		 % Periodo de símbolo
-L=10;		 % Número de bits a transmitir
-tipopulso=1; 	 %1: pulso rectangular
+% parámetros
+N=10;   % periodo do símbolo
+L=10;   % bits a mandar
+tipopulso=1;
 
-
-
-%=================== Generacion del pulso =========================
-
-if tipopulso == 1  %pulso rectangular
+% creación do pulso
+if tipopulso == 1
   n=0:N-1;
   pulso=ones(1,N);
-elseif tipopulso == 2 %escriba un elseif por cada tipo
+elseif tipopulso == 2
 end;
 
+% cálculo da enerxía
 
-%=================== Calculo de la energia del pulso =============
-%Escriba el codigo para calcular la energia
+% modulación
+bits=rand(1,L) < 0.5;
+% lazo que faga a seguinte asociación:
+%   A > 0 -> bit = 0
+%   A < 0 -> bit = 1
 
-%=================== Generacion de la senal (modulacion) =========
+% creación da señal recibida
+% señal recibida (transmitida + ruido)
 
-bits=rand(1,L) < 0.5; %genera 0 y 1 a partir de un vector de numeros
-                      %aleatorios con distribucion uniforme
+% probabilidade de erro
+% probabilidades teórica e real
 
-%Escriba un bucle que asocie un pulso con amplitud positiva a un bit 0 y
-%un pulso con amplitud negativa a un bit 1
-
-
-%=================== Generación de señal recibida  =============
-%Escriba el codigo para obtener la señal recibida (transmitida + ruido)
-
-%=================== Calculo de la proabilidad de error ===========
-%Escriba el codigo para calcular la probabilidad de error teórica y rea
-
-
-%=================== Representacion grafica ===================
+% representación gráfica
 figure(1)
 plot(n,pulso);
 axis([0 N -2 2])
 xlabel('t(s)')
-ylabel('Valor')
-title('Pulso transmitido: p(n)');
+ylabel('valor')
+title('pulso transmitido: p(n)');
 grid;
 
 figure(2)
 stem(n,pulso);
 axis([0 N -2 2])
 xlabel('t(s)')
-ylabel('Valor')
-title('Pulso transmitido: p(n)');
+ylabel('valor')
+title('pulso transmitido: p(n)');
 grid;
 
 figure(3)
 subplot(211)
-%Escriba el codigo para representar la senal transmitida sin ruido
-%utilize title, axis, xlabel e ylabel para ajustarla e identificarla correctamente
+% representar o sinal transmitido sen ruído (con title, axis, xlabel e ylabel)
 
 subplot(212)
-%Escriba el codigo para representar la senal recibida
-%utilize title, axis, xlabel e ylabel para ajustarla e identificarla correctamente
+% representar o sinal recibido (con title, axis, xlabel e ylabel)
 
 waitfor(gcf)
 
